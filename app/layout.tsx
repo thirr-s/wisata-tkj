b51261e344473e87168f1a5afef481e740ef06ed
FatngatirBilek <fathirbimashabri@gmail.com>
2025-02-21 23:00:51 +0700

    add section and sejarah <3

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
+        <div className="flex-1">{children}</div>
         <Footer />
       </body>
     </html>
