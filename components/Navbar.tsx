d30044a839608094fe2de840cef1d981772aae88
FatngatirBilek <fathirbimashabri@gmail.com>
2025-02-22 13:42:13 +0700

    add scroll animation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,39 @@
+"use client";
 import Theme from "./Theme";
-import Link from "next/link";
 
 export default function Navbar() {
+  const scrolltoHash = function (element_id: string) {
+    const element = document.getElementById(element_id);
+    element?.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest",
+    });
+  };
   return (
-    <div className="navbar ">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">TKJ</a>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal ">
-          <li>
-            <Link href="#sejarah">Sejarah</Link>
-          </li>
-          <li>
-            <Link href="#card">Wisata</Link>
-          </li>
-          <li>
-            <Link href="#jam-operasional">Jam Operasional</Link>
-          </li>
-          <li>
-            <Theme />
-          </li>
-        </ul>
+    <div>
+      <div className="navbar ">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">TKJ</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal ">
+            <li>
+              <a onClick={() => scrolltoHash("sejarah")}>Sejarah</a>
+            </li>
+            <li>
+              <a onClick={() => scrolltoHash("card")}>Wisata</a>
+            </li>
+            <li>
+              <a onClick={() => scrolltoHash("jam-operasional")}>
+                Jam Operasional
+              </a>
+            </li>
+            <li>
+              <Theme />
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
